a9cbd552c86c6a154dccf3a2cf7651b860310afc
YuriKievBarreto <yurikievbarreto@gmail.com>
2025-03-24 19:43:39 -0300

    finalizando estrutura inicial da pagina de cadastro

---
+++ public/js/previaCadastro.js
@@ -0,0 +1,19 @@
+const btnPrevia = document.getElementById('previa')
+
+btnPrevia.addEventListener('click', e =>{
+    //inputs
+    const inpNome = document.getElementById('inpNome')
+    const inpValor = document.getElementById('inpValor')
+    const inpImg = document.getElementById('inpImg')
+
+
+    //previas
+    const previaNome = document.getElementById('pNome')
+    const previaValor = document.getElementById('pValor')
+    const previaImg = document.getElementById('pImg')
+    console.log('apertei')
+
+    previaImg.src = inpImg.value
+    previaNome.innerHTML = inpNome.value
+    previaValor.innerHTML = `R$:${inpValor.value}`
+})
