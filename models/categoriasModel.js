4b1c6413f53bfae3c310e548be9d673c210199f9
YuriKievBarreto <yurikievbarreto@gmail.com>
2025-04-02 22:58:46 -0300

    mudando a modelagem dasa categorias

--- models/categoriasModel.js
+++ models/categoriasModel.js
@@ -1,12 +1,23 @@
 const mongoose = require('mongoose')
+//modelo a ser aplicado
+/*const categoriasSchema = new mongoose.Schema({
+   nome: String,
+   NomeDeExibicao: String,
+   subcategorias:{
+      sub1: String,
+      sub2: String,
+      sub3: String
+   } 
+})*/
 
 const categoriasSchema = new mongoose.Schema({
    categorias:[]
-       
-       
-   
+      
 })
+
+
 const Categorias = mongoose.model('Categorias', categoriasSchema);
+
 
 module.exports = Categorias
 
